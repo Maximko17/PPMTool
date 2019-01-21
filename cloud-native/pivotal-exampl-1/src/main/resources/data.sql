9ed68a7f516d5e2d3cc14d9ccd7c23adae9f3acd
Maxim <maxim06122000@mail.ru>
2019-01-21 15:41:47 +0300

    PPMTool

--- cloud-native/pivotal-exampl-1/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into customer values (1,'Maxim','Sukhodolets');
-insert into customer values (2,'Igor','Tarasov');
-insert into customer values (3,'Aleksandr','Smolin');
--- realcloudapp/src/main/java/maxcompany/realcloudapp/RealcloudappApplication.java
+++ realcloudapp/src/main/java/maxcompany/realcloudapp/RealcloudappApplication.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package maxcompany.realcloudapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RealcloudappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RealcloudappApplication.class, args);
     }
+
 }
 
