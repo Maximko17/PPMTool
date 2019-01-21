1d5bd510c290c2f4b9ec20064d695b3fbcd7490c
Maxim <maxim06122000@mail.ru>
2019-01-21 17:54:35 +0300

    "Full validation"

---
+++ realcloudapp/src/main/java/maxcompany/realcloudapp/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,13 @@
+package maxcompany.realcloudapp.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+}
